cdc4f9b41bee4080c316fbadbbfe131747e03f87
JoshiSankar-4021 <joshisankar5051@gmail.com>
2025-11-26 14:35:41 +0530

    fetching post

--- src/Forms/Forms.jsx
+++ src/Forms/Forms.jsx
@@ -2,6 +2,7 @@ import {useState} from "react";
 import './Forms.css';
 function Forms(){
     const [form,setForm]=useState({
+        firstname:"",
         email:"",
         password:""
     });
@@ -14,6 +15,13 @@ function Forms(){
     const handleSubmit=(e)=>{
         e.preventDefault();
         console.log(form);
+        fetch("http://localhost:3000/api/here2",{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify(form)
+        }).then(response=>response.json({message:"USER ADDED SUCESFULLY"}))
     }
 
     return(
@@ -21,14 +29,14 @@ function Forms(){
             <h1>This is Form Component</h1>
             <div className="form-div">
                 <form>
+                <lable className='form-lable'>First Name</lable>
+                <input type="email" className='form-input' name="firstname" value={form.firstname} onChange={handleChanges}/>
                 <lable className='form-lable'>Email</lable>
                 <input type="email" className='form-input' name="email" value={form.email} onChange={handleChanges}/>
                 <lable  className='form-lable'>Password</lable>
                 <input type="password" className='form-input' name="password" value={form.password} onChange={handleChanges}/>
                 <button className="submit" onClick={handleSubmit}>Submit</button>
             </form>
-            <h1>{form.email}</h1>
-            <h1>{form.password}</h1>
             </div>
         </div>
     );
--- src/Rendering/Rendering.jsx
+++ src/Rendering/Rendering.jsx
@@ -1,4 +1,5 @@
 import './Rendering.css';
+import { useEffect,useState } from 'react';
 function Rendering(){
     const products =[{name:"Shoes",brand:"Nike",productsno:10,id:1},
         {name:"Shoes",brand:"Paragon",productsno:10,id:2},
@@ -6,6 +7,21 @@ function Rendering(){
         {name:"Shoes",brand:"VKC PRIDE",productsno:10,id:4},
         {name:"Shoes",brand:"JIMMEY CHEW",productsno:10,id:5}
     ]
+    const [users,setUsers]=useState([]);
+    useEffect(() => {
+    fetch("http://localhost:3000/api/here2/1")
+        .then(res => res.json())
+        .then(data => {
+            setUsers(data.users);
+        })
+        .catch(err => console.log(err));
+    }, []);
+
+
+    useEffect(() => {
+        console.log(users);
+    }, [users]);
+
     return(
         <div>
             <h1>Dtat Rendering</h1>
@@ -53,6 +69,44 @@ function Rendering(){
                     </div>
                 ))
             }
+
+            <div>
+                <h1>
+                    DYNAMIC LIST RENDERING
+                </h1>
+                <ul>
+                {
+                    users.map((user)=>(
+                        <li key={user.id}>
+                            {user.firstname},{user.email},{user.password}
+                        </li>
+                    ))
+                }
+            </ul>
+            </div>
+            <div>
+                <h1>DYNAMIC TABLE RENDERING</h1>
+                 <table border="1">
+                <thead>
+            <tr>
+                <th>Id</th>
+                <th>FIRST NAME</th>
+                <th>EMAIL</th>
+                <th>PASSWORD</th>
+            </tr>
+                </thead>
+                {
+                    users.map((user)=>(
+                        <tr key={user.id}>
+                            <td>{user.id}</td>
+                            <td>{user.firstname}</td>
+                            <td>{user.email}</td>
+                            <td>{user.password}</td>
+                        </tr>
+                    ))
+                }
+            </table>
+            </div>
         </div>
     );
 }
